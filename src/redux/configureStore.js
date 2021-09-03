648f62aa8c45ed111df7cae67032244c0bd8de72
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-09-03 22:04:29 +0100

    remove redux logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducers = combineReducers({
   books: booksReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(logger, thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
